6ed781ca3296603eb2d00abc648f210d5dfdab00
Thomas Reitmayr <treitmayr@devbase.at>
2024-01-18 23:00:00 +0100

    Finally get it working

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 This project provides a UART to syslog gateway for up to two individually configurable UART ports. The syslog output supports [RFC 5424](https://datatracker.ietf.org/doc/html/rfc5424#section-6) UDP frames as well as raw UDP packets per text line received from the serial port(s). The software's intended purpose is to capture console output of embedded systems in a continuous and comfortable way for analysis or archiving.
 
 I currently use it to capture the console output of an AnkerMake M5C 3D printer, which logs via its serial line at 3 Mbaud. The included configuration file `sdkconfig.esp32dev-ankermake` is provided for that purpose.
+
+### Technical Note
+
+The allocation of CPU cores was carefully chosen in order to allow the ESP32 to keep track with burst of messages, which can
+carry a significant number of bytes and lines. For the AnkerMake M5C this is especially true during booting, but with the following
+CPU affinity of OS tasks, the 'FIFO full' events were eliminated:
+
+* CORE 0: UART driver and application code for processing serial data
+* CORE 1: Wifi driver and LwIP stack
--- src/main.c
+++ src/main.c
@@ -16,6 +16,7 @@
 #include "esp_system.h"
 #include "esp_event.h"
 #include "esp_log.h"
+#include "esp_wifi.h"
 
 #include "sdkconfig.h"
 #include "wifi_helper.h"
@@ -47,7 +48,20 @@ typedef struct
     QueueHandle_t uart_queue;
 } task_params_t;
 
-static QueueHandle_t uart1_queue;
+static char *app_name;
+
+// from https://stackoverflow.com/a/32496721
+static char* replace_char(char* str, char find, char replace)
+{
+    char *current_pos = strchr(str, find);
+    while (current_pos)
+    {
+        *current_pos = replace;
+        current_pos = strchr(current_pos, find);
+    }
+    return str;
+}
+
 
 static void uart_event_task(void *pvParameters)
 {
@@ -58,13 +72,17 @@ static void uart_event_task(void *pvParameters)
     task_params_t *params = (task_params_t *)pvParameters;
 
     char *buffer = build_syslog_client_header(SYSLOG_INFO,
-                                              CONFIG_SYSLOG_APP_NAME,
+                                              app_name,
                                               params->syslog_task_name);
     size_t header_len = strlen(buffer);
     buffer = realloc(buffer, header_len + LINE_BUF_SIZE + PATTERN_CHR_NUM + 1);
+    assert(buffer != NULL);
     char *msg = buffer + header_len;
+    *msg = '\0';
 
     vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    ESP_LOGI(TAG, "Capturing UART%d with syslog header '%s'", params->uart_port, buffer);
 
     error_msg = "[start uart console logging]";
     ESP_LOGI(TAG, "%s", error_msg);
@@ -81,33 +99,21 @@ static void uart_event_task(void *pvParameters)
                 //uart_get_buffered_data_len(params->uart_port, &buffered_size);
                 int pos = uart_pattern_pop_pos(params->uart_port);
                 //ESP_LOGD(TAG, "[UART PATTERN DETECTED] pos: %d", pos);
-                if (pos >= 0)
+                while (pos > LINE_BUF_SIZE)
                 {
-                    uart_read_bytes(params->uart_port, msg, pos + PATTERN_CHR_NUM, 100 / portTICK_PERIOD_MS);
-                    if (pos > 0)
-                    {
-                        while ((pos > 0) && (buffer[header_len + pos - 1] == '\r'))
-                        {
-                            // strip trailing carriage return
-                            pos -= 1;
-                        }
-                        if (pos > 0)
-                        {
-                            syslog_client_send_with_header(buffer, header_len + pos);
-                        }
-                    }
-                    //ESP_LOGD(TAG, "read data: %.*s", pos, msg);
+                    uart_read_bytes(params->uart_port, msg, LINE_BUF_SIZE, 100 / portTICK_PERIOD_MS);
+                    syslog_client_send_with_header(buffer, header_len + LINE_BUF_SIZE);
+                    pos -= LINE_BUF_SIZE;
                 }
-                else
+                uart_read_bytes(params->uart_port, msg, pos + PATTERN_CHR_NUM, 100 / portTICK_PERIOD_MS);
+                // strip trailing carriage return(s)
+                while ((pos > 0) && (msg[pos - 1] == '\r'))
                 {
-                    // the pattern position queue is full so that it cannot
-                    // record the position. We should set a larger queue size.
-                    const char *error_msg = "[Pattern position buffer full, expect missing lines!]";
-                    ESP_LOGW(TAG, "%s", error_msg);
-                    strcpy(msg, error_msg);
-                    syslog_client_send_with_header(buffer, -1);
-                    // As an example, we directly flush the rx buffer here.
-                    uart_flush_input(params->uart_port);
+                    pos -= 1;
+                }
+                if (pos > 0)
+                {
+                    syslog_client_send_with_header(buffer, header_len + pos);
                 }
                 break;
             //Event of HW FIFO overflow detected
@@ -120,7 +126,7 @@ static void uart_event_task(void *pvParameters)
                 syslog_client_send_with_header(buffer, -1);
                 // As an example, we directly flush the rx buffer here in order to read more data.
                 uart_flush_input(params->uart_port);
-                xQueueReset(uart1_queue);
+                xQueueReset(params->uart_queue);
                 break;
             //Event of UART ring buffer full
             case UART_BUFFER_FULL:
@@ -131,7 +137,7 @@ static void uart_event_task(void *pvParameters)
                 syslog_client_send_with_header(buffer, -1);
                 // As an example, we directly flush the rx buffer here in order to read more data.
                 uart_flush_input(params->uart_port);
-                xQueueReset(uart1_queue);
+                xQueueReset(params->uart_queue);
                 break;
             //Event of UART RX break detected
             case UART_BREAK:
@@ -142,6 +148,8 @@ static void uart_event_task(void *pvParameters)
                     strcpy(msg, error_msg);
                     syslog_client_send_with_header(buffer, -1);
                 }
+                uart_flush_input(params->uart_port);
+                xQueueReset(params->uart_queue);
                 break;
             //Event of UART frame error
             case UART_FRAME_ERR:
@@ -166,7 +174,7 @@ static void uart_event_task(void *pvParameters)
     vTaskDelete(NULL);
 }
 
-void configure_uart(uart_port_t uart_port, int baud_rate, int rx_io_num, const char *syslog_task_name)
+void configure_uart(uart_port_t uart_port, int baud_rate, int rx_io_num, const char *syslog_task_name, const char *os_task_name)
 {
     /* Configure parameters of an UART driver,
      * communication pins and install the driver */
@@ -181,11 +189,11 @@ void configure_uart(uart_port_t uart_port, int baud_rate, int rx_io_num, const c
     };
     QueueHandle_t uart_queue;
     // install UART driver, and get the queue.
-    uart_driver_install(uart_port, UART_BUF_SIZE, 0, UART_QUEUE_SIZE, &uart_queue, ESP_INTR_FLAG_IRAM);
-    uart_param_config(uart_port, &uart_config);
+    ESP_ERROR_CHECK(uart_driver_install(uart_port, UART_BUF_SIZE, 0, UART_QUEUE_SIZE, &uart_queue, ESP_INTR_FLAG_IRAM));
+    ESP_ERROR_CHECK(uart_param_config(uart_port, &uart_config));
 
-    // change receive FIFO "full" threshold from 0x60 (default) to 0x3e
-    uart_set_rx_full_threshold(uart_port, UART_RXFIFO_FULL_THRHD_V / 2);
+    // reduce receive FIFO "full" threshold from 0x60 (default) to reduce FIFO overflows
+    uart_set_rx_full_threshold(uart_port, UART_RXFIFO_FULL_THRHD_V / 3);
 
     // set UART pins
     uart_set_pin(uart_port, UART_PIN_NO_CHANGE,
@@ -203,10 +211,11 @@ void configure_uart(uart_port_t uart_port, int baud_rate, int rx_io_num, const c
     params->uart_port = uart_port;
     params->uart_queue = uart_queue;
     params->syslog_task_name = syslog_task_name;
-    char *pcname = "uartX_event_task";
-    pcname[4] = '0' + uart_port;
-    xTaskCreate(uart_event_task, pcname, 2048, (void *)params, 12, NULL);
+    // run our task on the CPU core not running the Wifi driver
+    BaseType_t cpu_affinity = configNUM_CORES - 1 - WIFI_TASK_CORE_ID;
+    xTaskCreatePinnedToCore(uart_event_task, os_task_name, 2048, (void *)params, 12, NULL, cpu_affinity);
 }
+
 
 void app_main(void)
 {
@@ -231,17 +240,32 @@ void app_main(void)
 
     syslog_client_start(CONFIG_SYSLOG_HOST, CONFIG_SYSLOG_PORT, SYSLOG_LOCAL0);
 
+    app_name = strdup(CONFIG_SYSLOG_APP_NAME);
+    (void) replace_char(app_name, ' ', '_');
+
 #ifdef CONFIG_SYSLOG_USE_UART1
-    configure_uart(UART_NUM_1,
-                   CONFIG_SYSLOG_UART1_BAUD_RATE,
-                   CONFIG_SYSLOG_UART1_RX_PIN,
-                   CONFIG_SYSLOG_UART1_TASK_NAME);
+    {
+        char *task_name = strdup(CONFIG_SYSLOG_UART1_TASK_NAME);
+        (void) replace_char(task_name, ' ', '_');
+
+        configure_uart(UART_NUM_1,
+                       CONFIG_SYSLOG_UART1_BAUD_RATE,
+                       CONFIG_SYSLOG_UART1_RX_PIN,
+                       task_name,
+                       "uart1_event_task");
+    }
 #endif
 
 #ifdef CONFIG_SYSLOG_USE_UART2
-    configure_uart(UART_NUM_2,
-                   CONFIG_SYSLOG_UART2_BAUD_RATE,
-                   CONFIG_SYSLOG_UART2_RX_PIN,
-                   CONFIG_SYSLOG_UART2_TASK_NAME);
+    {
+        char *task_name = strdup(CONFIG_SYSLOG_UART2_TASK_NAME);
+        (void) replace_char(task_name, ' ', '_');
+
+        configure_uart(UART_NUM_2,
+                       CONFIG_SYSLOG_UART2_BAUD_RATE,
+                       CONFIG_SYSLOG_UART2_RX_PIN,
+                       task_name,
+                       "uart2_event_task");
+    }
 #endif
 }
